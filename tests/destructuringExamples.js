7319fa033ac1bc0a246a423eb85f76b64ba295d6
Owain Llewellyn <Owain.Llewellyn@akqa.com>
2016-03-22 09:39:14 -0400

    Added latest files

--- tests/destructuringExamples.js
+++ tests/destructuringExamples.js
@@ -14,9 +14,23 @@ test('Destructuring', (t) => {
 		}
 	}
 
-	var [a,b,c] = foo();
-	var {x: x, y:y, z:z} = bar();
+	let [a,b,c] = foo();
+	let {x: x, y:y, z:z} = bar();
 
 	t.same(foo(), [1,2,3]);
 	t.same(bar(), {x: 4, y: 5, z: 6});
+});
+
+test('Basic destructuring assignment', (t) => {
+	t.plan(5);
+	
+	let foo = [1,2,3,4,5];
+
+	var [one, two, three, four, five] = foo;
+
+	t.is(one, 1);
+	t.is(two, 2);
+	t.is(three, 3);
+	t.is(four, 4);
+	t.is(five, 5);
 });
